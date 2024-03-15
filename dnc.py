561106b02bef666acbc02e16f651b6a05fecd9b3
Haikalin <haikalassyauqi70@gmail.com>
2024-03-15 14:49:24 +0700

    selesai bos

--- dnc.py
+++ dnc.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-import numpy as np
 import bruteforce
 
 def midpoint(x1,y1,x2,y2) :
@@ -25,15 +24,22 @@ def expandarray(array1, array2) :
         array1.append(array2[i])
     return array1
 
-titikawal = []
-titikakhir = []
-titikbantu = []
-titikbezier = []
-xawal = []
-yawal = []
+def animasi(array) :
+    plt.clf()
+    plt.scatter([i[0] for i in array],[i[1] for i in array])
+    plt.plot([i[0] for i in array],[i[1] for i in array])
+    plt.pause(2)
+
+titikawal = [] #Titik P0
+titikakhir = [] #Titik Pn
+titikbantu = [] #Semua titik kontrol + titik kurva
+titikbezier = [] # Titik kurva
+xawal = [] # X titik yang masuk di awal
+yawal = [] # Y titik yang masuk di awal
 
 n = int(input("Masukkan banyak garis: "))
 algo = int(input("Masukkan algoritma: "))
+
 
 for i in range(n+1) :
     titik = []
@@ -58,6 +64,7 @@ if algo == 1 :
     y = []
     y.append(titikawal[0][1])
     iterasi = int(input("Masukkan iterasi: "))
+    iterasi = 2**iterasi - 1
     t = 1/iterasi
     while t < 1 :
         titik = bruteforce.bexierbruteforce(titikbantu, t)
@@ -68,6 +75,7 @@ if algo == 1 :
 if algo == 2:
     iterasi = int(input("Masukkan iterasi: "))
     for i in range(iterasi) :
+        titikbezier = []
         titikdipakai = 0
         titikbantutemp = titikbantu
         titikbantu = []
@@ -88,9 +96,9 @@ if algo == 2:
             temp.append(titikbantutemp[titikdipakai])
             titikbantu = expandarray(titikbantu,temp)
         titikbantu.insert(0,titikawal[0])
-
-    for i in range(0,len(titikbantu),n) :
-        titikbezier.append(titikbantu[i])
+        for i in range(0, len(titikbantu), n) :
+            titikbezier.append(titikbantu[i])
+        animasi(titikbezier)
 
     x = []
     y = []
@@ -98,6 +106,7 @@ if algo == 2:
         x.append(titikbezier[i][0])
         y.append(titikbezier[i][1])
 
-plt.plot(xawal,yawal)
+plt.clf()
 plt.plot(x,y)
+plt.scatter(x,y)
 plt.show()
